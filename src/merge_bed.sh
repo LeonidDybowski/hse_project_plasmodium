06906438ed5018044dcbb9950cba19bd083f3b18
LeonidDybovskij <60808642+LeonidDybovskij@users.noreply.github.com>
2022-06-18 17:18:41 +0300

    Delete merge_bed.sh

--- src/merge_bed.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for filename in /home/leonid/PycharmProjects/python/minor_2022/project/*.bed; do
-    sort -k1,1 -k2,2n $filename > sorted_$filename
-    bedtools merge -i sorted_$filename  -c 5 -o max > merged_$filename.bed
-done
-
